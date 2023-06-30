b66f1cdc0d69fc200c8c9031301a28d9ac3c5227
CaioSC01 <caio180g@gmail.com>
2023-06-30 01:37:21 -0300

    Começando com a seção de Inicio do projeto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./components/header/Header";
+import Home from "./components/Home/Home";
 import "./App.css";
 
 const App = () => {
   return (
     <>
       <Header />
+      <main className="mais">
+        <Home />
+      </main>
     </>
   );
 };
