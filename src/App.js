a7e0644994610100ee608c5d65855c4442636c60
CaioSC01 <caio180g@gmail.com>
2023-06-30 00:30:22 -0300

    Adicionando a header dentro do escopo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./components/header/Header";
+import "./App.css";
 
 const App = () => {
   return (
     <>
-   
+      <Header />
     </>
   );
 };
