7416ec623f42be240df8a534ca0c27f19fd48401
CaioSC01 <caio180g@gmail.com>
2023-07-01 04:14:42 -0300

    Finalizando parte do inicio com o scroll

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./home.css";
 import Social from "./Social";
 import Data from "./Data";
+import ScrollDown from "./ScrollDown";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
             <div className="home__img"></div>
             <Data />
           </div>
+          <ScrollDown />
         </div>
       </section>
     </>
