0e88a527935369acb058890d0b053bb41d484e96
CaioSC01 <caio180g@gmail.com>
2023-07-02 03:17:29 -0300

    Adicionando Sobre Mim no projeto

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/header/Header";
 import Home from "./components/Home/Home";
+import About from "./components/About/About";
 import "./App.css";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <main className="main">
         <Home />
+        <About />
       </main>
     </>
   );
