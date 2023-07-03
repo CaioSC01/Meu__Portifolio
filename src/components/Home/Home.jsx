37f0ec1c7077e914930bc7dd07e4b986a2d03a6b
CaioSC01 <caio180g@gmail.com>
2023-07-03 19:21:03 -0300

    Arrumando ID

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ import ScrollDown from "./ScrollDown";
 const Home = () => {
   return (
     <>
-      <section className="home section" id="home">
+      <section className="home section" id="inicio">
         <div className="home__container container grid">
           <div className="home__content grid">
             <Social />
