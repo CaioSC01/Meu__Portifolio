02859c43c2ec64bbeac1b97b93f0d5ad83d3351d
CaioSC01 <caio180g@gmail.com>
2023-07-03 22:38:34 -0300

    Adicionando card front end

---
+++ src/components/Skills/Frontend.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+const Frontend = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Desenvolvedor frontend</h3>
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">HTML</h3>
+              <span className="skills__level">Intermediário</span>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">CSS</h3>
+              <span className="skills__level">Intermediário</span>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">React.js</h3>
+              <span className="skills__level">Intermediário</span>
+            </div>
+          </div>
+        </div>
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">Next.js</h3>
+              <span className="skills__level">Básico</span>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">Taiwind</h3>
+              <span className="skills__level">Básico</span>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">JavaScript</h3>
+              <span className="skills__level">Intermediário</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Frontend;
